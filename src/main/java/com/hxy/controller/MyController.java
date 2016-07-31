62a3c5360c4517e387e3e27dca1196cae4e571d2
hxy <oheasy2000@163.com>
2016-07-31 20:18:28 +0800

    add v

--- src/main/java/com/hxy/controller/MyController.java
+++ src/main/java/com/hxy/controller/MyController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -22,5 +24,17 @@ public class MyController {
         User user = userService.queryByName("hxy");
         map.put("user", user);
         return "index";
+    }
+
+    @RequestMapping(value = "/retuser", method = RequestMethod.GET)
+    @ResponseBody
+    public Map<String, String> retUser() {
+        User user = new User();
+        user.setName("houxiaoyu");
+        user.setAge(49);
+        HashMap<String, String> map = new HashMap<>();
+        map.put("key", "value");
+        map.put("key2", "value2");
+        return map;
     }
 }
