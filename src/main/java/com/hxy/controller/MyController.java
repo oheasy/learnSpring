c2b5f8d897a6a16fb3b19fe2413a554e4d36021a
hxy <oheasy2000@163.com>
2016-07-31 19:56:02 +0800

    commit 0731

--- src/main/java/com/hxy/controller/MyController.java
+++ src/main/java/com/hxy/controller/MyController.java
@@ -1,10 +1,13 @@
 package com.hxy.controller;
 
+import com.hxy.pojo.User;
 import com.hxy.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Map;
 
 /**
  * Created by hxy-pc on 2016/7/19.
@@ -14,8 +17,10 @@ public class MyController {
     @Autowired
     UserService userService;
 
-    @RequestMapping(value ="/index", method = RequestMethod.GET)
-    public String getIndex() {
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public String getIndex(Map map) {
+        User user = userService.queryByName("hxy");
+        map.put("user", user);
         return "index";
     }
 }
--- src/main/webapp/views/index.jsp
+++ src/main/webapp/views/index.jsp
@@ -1,5 +1,7 @@
 <html>
 <body>
 <h2>Hello World!</h2>
+${user.name}<br/>
+${user.age}
 </body>
 </html>
